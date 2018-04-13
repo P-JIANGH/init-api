e5d01472b9943148989f5438923bf28caa5e77f7
jianghao <jianghao@jntci.com.cn>
2018-04-13 16:11:15 +0800

    add logger

--- api/com/test/controller/TestContoller.java
+++ api/com/test/controller/TestContoller.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,15 @@ import com.test.services.TestService;
 @Controller
 @RequestMapping("/test")
 public class TestContoller {
+	
+	private static final Logger logger = Logger.getLogger(TestContoller.class);
 
 	@Autowired
 	private TestService testService;
 
 	@RequestMapping("/init")
 	public ResponseData test() {
+		logger.info("Into Test API!");
 		ResponseData data = new ResponseData();
 		data.setData(testService.getTime());
 		return data;
